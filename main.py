022a6e6e1bc44bc81ff1f857f541c7b1f399a1cb
Peyman Majidi <Peyman.majidi@gmail.com>
2019-04-19 08:24:18 +0430

    Debug

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os 
-import face_recognition 
+import face_recognition
+import random
 from PIL import Image, ImageDraw, ImageFont
 from tkinter import filedialog
 
@@ -15,40 +16,46 @@ for filename in os.listdir("./known"):
         name=os.path.splitext(filename)[0]  #filename is the refrence for who is in the picture: Bill.jpg means Bill :)
         try:
             encoded_face = face_recognition.face_encodings(face)[0] # we know only 1 face is the known folder
-            encoded_faces.append((encoded_face, name)) 
+            encoded_faces.append((encoded_face, name))
+            print(name)
         except:   # if there is no face in the picture it couse error we ignore the error but there is nothing to do
             pass 
 
 face_locations=face_recognition.face_locations(image) 
 source_img = Image.open(source).convert("RGBA")
 
+
 draw=ImageDraw.Draw(source_img)
 for x1, y1, x2, y2 in face_locations: 
-    font=ImageFont.Truetype("arial.ttf", 35, encoding="unic") 
+    font=ImageFont.truetype("arial.ttf", 35, encoding="unic") 
     point=((y2, x2, y1, x1)) 
     a1=max(x1, x2) 
     b1=max(y1, y2) 
     a2=min(x1, x2) 
-    b2=min(y1, y2) 
+    b2=min(y1, y2)
     me=source_img.crop((b2, a2, b1, a1)) 
-    me.save("./temp.png","PNG") 
+
+    # r = random.randint(100,10000)
+    # me.save(f"./temp{r}.png","PNG") # save each face in a png file
+
+    me.save(f"./temp.png","PNG") 
     unknown_picture=face_recognition.load_image_file("./temp.png") 
     unknown_face_encoding=face_recognition.face_encodings(unknown_picture) 
     found=False 
     try: 
-        for face, name in encoded_faces: 
-            results=face_recognition.compare_faces([face],unknown_face_encoding[o], tolerance=5) 
+        for f, name in encoded_faces: 
+            results=face_recognition.compare_faces([f],unknown_face_encoding[0], tolerance=4) 
             if results[0]== True:
-                draw.text((y2, x2), name, font=font, fill=(0,255, O)) 
+                draw.text((y2, x2), name, font=font, fill=(0,255, 0)) 
                 found=True
                 break 
 
         if (found==False) : 
-           draw.text((y2, x2),"Unknown", font=font, fill=(255, O, O)) 
+           draw.text((y2, x2),"Unknown", font=font, fill=(255, 0, 0)) 
     except IndexError:
         pass
 
-    draw.rectangle (((y1, x1), (y2,x2) ), fill=None, outline=(0,255,0), width=7) 
+    draw.rectangle(((y1, x1), (y2,x2)), fill=None, outline=(0,255,0))
 
 #source_img.save("output. png","PNG") 
 source_img.show()
